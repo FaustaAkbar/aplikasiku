1223e6f80e179e3400ff9b0f09fa6ed8085c4a95
Fausta Akbar Wijaya <faussss1357@gmail.com>
2024-09-29 16:07:57 +0700

    Update main.dart
    New branch create and create new class for home page
    
    Co-Authored-By: Clowly <110012963+clowlyy@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -15,22 +15,31 @@ class app extends StatelessWidget {
               backgroundColor: Colors.blue,
               title: Text("Hallo"),
             ),
-            body: Center(
-                child: Text(
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec purus massa. Morbi varius diam turpis, ut sodales tortor congue sed. Morbi quis vehicula sem.",
-              //overflow: TextOverflow.ellipsis,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  backgroundColor: Colors.amber,
-                  color: Colors.black,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 9,
-                  fontFamily: 'Winter',
-                  decorationStyle: TextDecorationStyle.wavy,
-                  decoration: TextDecoration.underline,
-                  decorationColor: Colors.red,
-                  decorationThickness: 4),
-            ))));
+            body: HomePage()));
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Text(
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec purus massa. Morbi varius diam turpis, ut sodales tortor congue sed. Morbi quis vehicula sem.",
+      //overflow: TextOverflow.ellipsis,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+          backgroundColor: Colors.amber,
+          color: Colors.black,
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 9,
+          fontFamily: 'Winter',
+          decorationStyle: TextDecorationStyle.wavy,
+          decoration: TextDecoration.underline,
+          decorationColor: Colors.red,
+          decorationThickness: 4),
+    ));
   }
 }
